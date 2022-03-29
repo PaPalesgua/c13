d18ed6e2d2c35f83b222c88996445df4b8bdb8c4
PaPalesgua <101650831+PaPalesgua@users.noreply.github.com>
2022-03-29 11:19:26 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,115 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+var cloud, cloudsGroup, cloudImage;
+var obstaculos;
+var obstacle1,obstacle2,obstacle3,obstacle4,obstacle5,obstacle6
+
+var newImage;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+  obstacle1 = loadImage("obstacle1.png")
+  obstacle2 = loadImage("obstacle2.png")
+  obstacle3 = loadImage("obstacle3.png")
+  obstacle4 = loadImage("obstacle4.png")
+  obstacle5 = loadImage("obstacle5.png")
+  obstacle6 = loadImage("obstacle6.png")
+
+
+
+
+}
+
+function setup() {
+  createCanvas(600, 200);
+
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  // trex.addAnimation("collided",trex_collided)
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  console.log("Hello"+ 5)
+  
+}
+
+function draw() {
+  background(180);
+  
+  
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  trex.collide(invisibleGround);
+  
+  //gerar as nuvens
+  spawnClouds();
+  spawnObstacles()
+  drawSprites();
+}
+
+function spawnClouds() {
+  //escreva o código aqui para gerar as nuvens
+  if (frameCount % 60 === 0) {
+    cloud = createSprite(600,100,40,10);
+    cloud.addImage(cloudImage)
+    cloud.y = Math.round(random(10,60))
+    cloud.scale = 0.4;
+    cloud.velocityX = -3;
+    
+    
+    //atribua tempo de vida à variável
+    cloud.lifetime = 600/3
+    
+    //ajuste a profundidade
+    cloud.depth = trex.depth
+    trex.depth = trex.depth + 1;
+    }
+}
+
+  function spawnObstacles() {
+    if (frameCount % 60 === 0) {
+      obstaculos = createSprite(400,165,10,40);
+      obstaculos.velocityX=-6
+      var ram = Math.round(random(1,6));
+     switch (ram){  
+         case 1: obstaculos.addImage(obstacle1);
+                 break;
+         case 2: obstaculos.addImage(obstacle2);
+                break;     
+         case 3: obstaculos.addImage(obstacle3);
+                 break;
+         case 4: obstaculos.addImage(obstacle4);
+                 break;
+         case 5: obstaculos.addImage(obstacle5);
+                 break;
+         case 6: obstaculos.addImage(obstacle6);
+                 break;
+         default: break;  
+      
+        }
+        console.log(ram)
+        obstaculos.scale = 0.7
+        obstaculos.lifetime = 400/6
+      }
+  }
